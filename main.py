dfe1d820c3f50bf99792c6b55e118d507162ea99
chaochen83 <chaochen83@163.com>
2025-04-01 22:18:46 +0800

    Update notificaitons

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ API_KEY: Final = os.getenv("API_KEY")
 API_URL: Final = "https://dashscope.aliyuncs.com/api/v1/services/aigc/image-generation/generation"
 CALLBACK_URL: Final = "https://dashscope.aliyuncs.com/api/v1/tasks/"
 
+# Change this to the image url that you want to ref its style:
 style_ref_url = "https://pics6.baidu.com/feed/29381f30e924b899f3543621c3ae869a0b7bf6f3.jpeg@f_auto?token=cfc086a746067ded623308bf9a673c79"
+# Change this to the original image's url:
 origin_image_url = "https://public-vigen-video.oss-cn-shanghai.aliyuncs.com/public/dashscope/test.png"
 
 async def poll_status(task_id: str, max_retries: int = 5, delay: float = 3.0):
@@ -22,11 +24,11 @@ async def poll_status(task_id: str, max_retries: int = 5, delay: float = 3.0):
         url = CALLBACK_URL + task_id
         async with httpx.AsyncClient(headers={"Authorization": "Bearer " + API_KEY}) as client:
             for _ in range(max_retries):
-                print(f"Polling status for task ID: {task_id}")
+                print(f"Polling status for task ID: {task_id}\n")
                 response = await client.get(url)
                 if response.status_code == 200:
                     response_json = response.json()
-                    print(f"Response: {response_json}")
+                    print(f"Response: {response_json}\n")
                     if response_json['output']['task_status'] == "SUCCEEDED":
                         return response_json['output']['results'][0]['url']  # 任务完成，返回数据
                 await asyncio.sleep(delay)  # 休眠后重试
@@ -48,22 +50,22 @@ async def start_request():
                     
                 }
             }
-            print("Sending request...")
+            print("Sending request...\n")
             response = await client.post(API_URL, json=payload)
             # print(f"create image Response : {response.text}")
             if response.status_code == 200:
                 response_json = response.json()
                 task_id = response_json['output']['task_id']
-                print(f"Task ID: {task_id}")
+                print(f"Task ID: {task_id}\n")
                 await asyncio.sleep(3)  # Wait for 3 seconds before sending the request
-                print("Polling task status...")
+                print("Polling task status...\n")
                 # 轮询查询任务状态
                 return await poll_status(task_id)
             else:
-                return(f"Request failed with err msg: {response.text}")
+                return(f"Request failed with err msg: {response.text}\n")
     except httpx.RequestError as e:
         return(f"HTTP error occurred: {e.response.status_code} - {e.response.text}")
     
-
+# This is the target image's url:
 result_image_url = asyncio.run(start_request())
 print(result_image_url)
